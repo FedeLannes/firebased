42f3fde23bdee63e0904a8319eb8485a488dd2d0
FedeLannes <matiaslannes64@gmail.com>
2021-12-17 13:54:42 -0300

    push a git

---
+++ android/app/src/main/kotlin/com/example/firebased/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.universidadtecologicatucuman.firebasedxdd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,27 @@
+// @dart=2.9
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebased/models/user.dart';
+import 'package:firebased/screens/calendar.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  MyUser _userFromFirebaseUser(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
+
+  Stream<MyUser> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
